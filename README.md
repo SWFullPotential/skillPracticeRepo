6f3f4460e798372aced64fd555b1a8f2c74d7176
SWFullPotential <swfullpotential@gmail.com>
2020-11-29 21:08:23 -0700

    continuing JS cert

---
+++ README.md
@@ -0,0 +1,1 @@
+This repository is where I will track my coding practice.
---
+++ codeCampJS.js
@@ -0,0 +1,18 @@
+let a = 5;
+let b = 1;
+a++;
+// Only change code below this line
+console.log(a);
+
+let sumAB = a + b;
+console.log(sumAB);
+//------------------
+// Open your browser console.
+let output =
+  "Get this to log once in the freeCodeCamp console and twice in the browser console";
+// Use console.log() to print the output variable.
+console.clear();
+console.log(output);
+// Run the tests to see the difference between the two consoles.
+
+// Now, add console.clear() before your console.log() to clear the browser console, and pass the tests.
