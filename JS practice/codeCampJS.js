4b6b2a6be8a3d992720bbe2399ea8c0f8d3ba18a
SWFullPotential <swfullpotential@gmail.com>
2020-12-01 21:08:43 -0700

    continuing JS course

--- JS practice/codeCampJS.js
+++ JS practice/codeCampJS.js
@@ -58,6 +58,15 @@ if (x == y) {
 console.log(result);
 
 //----------
+//Fix the code so the variable result is set to the value returned from calling the function getNine.
+function getNine() {
+  let x = 6;
+  let y = 3;
+  return x + y;
+}
+
+let result = getNine();
+console.log(result);
 
 //----------
 
