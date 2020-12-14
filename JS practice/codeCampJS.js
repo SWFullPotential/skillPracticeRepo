606af67784cc1f1262e8a2fd0486a5d4b1e88497
SWFullPotential <swfullpotential@gmail.com>
2020-12-14 14:49:39 -0700

    spread operator practice

--- JS practice/codeCampJS.js
+++ JS practice/codeCampJS.js
@@ -239,6 +239,15 @@ function copyMachine(arr, num) {
 console.log(copyMachine([true, false, true], 2));
 
 //----------
+//We have defined a function spreadOut that returns the variable sentence. Modify the function using the spread operator so that it returns the array ['learning', 'to', 'code', 'is', 'fun'].
+
+function spreadOut() {
+  let fragment = ["to", "code"];
+  let sentence = ["learning", ...fragment, "is", "fun"];
+  return sentence;
+}
+
+console.log(spreadOut());
 
 //----------
 
