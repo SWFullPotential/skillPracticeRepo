a990eada1b7a45540cd7610b8e62d8f29380c20b
SWFullPotential <swfullpotential@gmail.com>
2020-12-14 11:25:51 -0700

    used splice to remove elements from arr

--- JS practice/codeCampJS.js
+++ JS practice/codeCampJS.js
@@ -182,6 +182,12 @@ function popShift(arr) {
 console.log(popShift(["challenge", "is", "not", "complete"]));
 
 //----------
+//We've initialized an array arr. Use splice() to remove elements from arr, so that it only contains elements that sum to the value of 10.
+const arr = [2, 4, 5, 1, 7, 5, 2, 1];
+// Only change code below this line
+arr.splice(1, 4);
+// Only change code above this line
+console.log(arr);
 
 //----------
 
