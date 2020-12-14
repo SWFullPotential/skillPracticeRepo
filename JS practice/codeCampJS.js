bc5ebd713f88f77d4afaeffd9b27dcdc7f63987f
SWFullPotential <swfullpotential@gmail.com>
2020-12-14 15:39:32 -0700

    practice with for loops and nested arrays

--- JS practice/codeCampJS.js
+++ JS practice/codeCampJS.js
@@ -250,8 +250,39 @@ function spreadOut() {
 console.log(spreadOut());
 
 //----------
+//indexOf() can be incredibly useful for quickly checking for the presence of an element on an array. We have defined a function, quickCheck, that takes an array and an element as arguments. Modify the function using indexOf() so that it returns true if the passed element exists on the array, and false if it does not.
 
+function quickCheck(arr, elem) {
+  if (arr.indexOf(elem) >= 0) return true;
+  else return false;
+}
+let arr = ["squash", "onions", "shallots"];
+let elem = "mushrooms";
+
+quickCheck(arr, elem);
 //----------
+//We have defined a function, filteredArray, which takes arr, a nested array, and elem as arguments, and returns a new array. elem represents an element that may or may not be present on one or more of the arrays nested within arr. Modify the function, using a for loop, to return a filtered version of the passed array such that any array nested within arr containing elem has been removed.
+function filteredArray(arr, elem) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].indexOf(elem) == -1) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
+
+console.log(
+  filteredArray(
+    [
+      [3, 2, 3],
+      [1, 6, 3],
+      [3, 13, 26],
+      [19, 3, 9],
+    ],
+    3
+  )
+);
 
 //----------
 
