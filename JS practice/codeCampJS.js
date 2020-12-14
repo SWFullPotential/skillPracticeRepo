a065437edac8ecb85f3367bd3a224396c4bb0675
SWFullPotential <swfullpotential@gmail.com>
2020-12-14 11:18:46 -0700

    Basic data structure practice

--- JS practice/codeCampJS.js
+++ JS practice/codeCampJS.js
@@ -152,6 +152,96 @@ function myFunc() {
 }
 
 //----------
+let yourArray = [1, "2", true, "up", 42];
+
+//----------
+let myArray = ["a", "b", "c", "d"];
+// Only change code below this line
+myArray[1] = "anything but 'b'";
+// Only change code above this line
+console.log(myArray);
+
+//----------
+function mixedNumbers(arr) {
+  // Only change code below this line
+  arr.unshift("I", 2, "three");
+  arr.push(7, "VIII", 9);
+  // Only change code above this line
+  return arr;
+}
+
+console.log(mixedNumbers(["IV", 5, "six"]));
+
+//----------
+function popShift(arr) {
+  let popped = arr.pop();
+  let shifted = arr.shift();
+  return [shifted, popped];
+}
+
+console.log(popShift(["challenge", "is", "not", "complete"]));
+
+//----------
+
+//----------
+
+//----------
+
+//----------
+
+//----------
+
+//----------
+
+//----------
+
+//----------
+
+//----------
+
+//----------
+
+//----------
+
+//----------
+
+//----------
+
+//----------
+
+//----------
+
+//----------
+
+//----------
+
+//----------
+
+//----------
+
+//----------
+
+//----------
+
+//----------
+
+//----------
+
+//----------
+
+//----------
+
+//----------
+
+//----------
+
+//----------
+
+//----------
+
+//----------
+
+//----------
 
 //----------
 
