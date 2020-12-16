5de0941e64e2b734aedf9ad215b0e9ecf5a3a679
SWFullPotential <swfullpotential@gmail.com>
2020-12-16 13:13:28 -0700

    algorithm scripting practice

--- JS practice/codeCampJS.js
+++ JS practice/codeCampJS.js
@@ -512,7 +512,29 @@ function findLongestWordLength(str) {
 
 findLongestWordLength("The quick brown fox jumped over the lazy dog");
 //----------
+//Return an array consisting of the largest number from each provided sub-array. For simplicity, the provided array will contain exactly 4 sub-arrays. Remember, you can iterate through an array with a simple for loop, and access each member with array syntax arr[i].
+function largestOfFour(arr) {
+  const newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    // console.log(arr[i])
+    let largestNum = arr[i][0];
+    for (let sub = 0; sub < arr[i].length; sub++) {
+      if (largestNum < arr[i][sub]) {
+        largestNum = arr[i][sub];
+        // console.log(largestNum)
+      }
+    }
+    newArr.push(largestNum);
+  }
+  return newArr;
+}
 
+largestOfFour([
+  [4, 5, 1, 3],
+  [13, 27, 18, 26],
+  [32, 35, 37, 39],
+  [1000, 1001, 857, 1],
+]);
 //----------
 
 //----------
