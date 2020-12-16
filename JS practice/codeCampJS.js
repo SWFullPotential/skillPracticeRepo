73a95099b86563c47bdd53c8301ddeee927059dc
SWFullPotential <swfullpotential@gmail.com>
2020-12-16 12:42:44 -0700

    algorithm scripting practice

--- JS practice/codeCampJS.js
+++ JS practice/codeCampJS.js
@@ -497,7 +497,20 @@ function factorialize(num) {
 
 factorialize(5);
 //----------
+//Return the length of the longest word in the provided sentence.
 
+function findLongestWordLength(str) {
+  str = str.split(" ");
+  let longestWord = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (longestWord < str[i].length) {
+      longestWord = str[i].length;
+    }
+  }
+  return longestWord;
+}
+
+findLongestWordLength("The quick brown fox jumped over the lazy dog");
 //----------
 
 //----------
