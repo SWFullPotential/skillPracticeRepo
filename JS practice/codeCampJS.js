2dcf1c26ed88de8a1b9eabb3013a9db703c05073
SWFullPotential <swfullpotential@gmail.com>
2020-12-16 15:18:57 -0700

    algorithm scripting practice

--- JS practice/codeCampJS.js
+++ JS practice/codeCampJS.js
@@ -561,7 +561,22 @@ function repeatStringNumTimes(str, num) {
 
 repeatStringNumTimes("abc", 3);
 //----------
+//Truncate a string (first argument) if it is longer than the given maximum string length (second argument). Return the truncated string with a ... ending.
 
+function truncateString(str, num) {
+  let truncStr = "";
+  if (num < str.length) {
+    truncStr = str.slice(0, num) + "...";
+  } else {
+    truncStr = str.slice(0, num);
+  }
+  return truncStr;
+}
+
+truncateString(
+  "A-tisket a-tasket A green and yellow basket",
+  "A-tisket a-tasket A green and yellow basket".length
+);
 //----------
 
 //----------
