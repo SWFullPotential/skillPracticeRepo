06c275f873bef25cff7ef320365c4b3271e3a476
SWFullPotential <swfullpotential@gmail.com>
2020-12-16 11:44:25 -0700

    algorithm scripting practice

--- JS practice/codeCampJS.js
+++ JS practice/codeCampJS.js
@@ -469,7 +469,14 @@ function addFriend(userObj, friend) {
 
 console.log(addFriend(user, "Pete"));
 //----------
+//You are given a variable celsius representing a temperature in Celsius. Use the variable fahrenheit already defined and assign it the Fahrenheit temperature equivalent to the given Celsius temperature. Use the algorithm mentioned above to help convert the Celsius temperature to Fahrenheit.
 
+function convertToF(celsius) {
+  let fahrenheit = (celsius * 9) / 5 + 32;
+  return fahrenheit;
+}
+
+convertToF(30);
 //----------
 
 //----------
