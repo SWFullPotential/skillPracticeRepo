143764d92e54506a7749d495363bdb25ccbc4cd3
SWFullPotential <swfullpotential@gmail.com>
2020-12-16 12:18:52 -0700

    algorithm scripting practice

--- JS practice/codeCampJS.js
+++ JS practice/codeCampJS.js
@@ -486,7 +486,16 @@ function reverseString(str) {
 
 reverseString("hello");
 //----------
+//Understanding recursion: Recursion refers to a function repeating (calling) itself. In this case we are basically returning the given number (i.e. 5), multiplied by the function itself but this time the value passed to the num parameter is num-1 (which initially translates to 4). The very function is going to run inside itself interesting, eh?
 
+function factorialize(num) {
+  if (num === 0) {
+    return 1;
+  }
+  return num * factorialize(num - 1);
+}
+
+factorialize(5);
 //----------
 
 //----------
