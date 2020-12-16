673862eef1a83d6c526a7da7c95966047c73adf8
SWFullPotential <swfullpotential@gmail.com>
2020-12-16 16:26:46 -0700

    algorithm scripting practice

--- JS practice/codeCampJS.js
+++ JS practice/codeCampJS.js
@@ -594,7 +594,17 @@ function booWho(bool) {
 
 booWho(true);
 //----------
+/*Return the provided string with the first letter of each word capitalized. Make sure the rest of the word is in lower case. (For the purpose of this exercise, you should also capitalize connecting words like "the" and "of".) */
 
+function titleCase(str) {
+  let splitStr = str.toLowerCase().split(" ");
+  let upCase = splitStr.map(function (val) {
+    return val.replace(val.charAt(0), val.charAt(0).toUpperCase());
+  });
+  return upCase.join(" ");
+}
+
+titleCase("I'm a little tea pot");
 //----------
 
 //----------
