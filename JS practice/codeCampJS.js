3fafa9cbbca6d3dc61b161374cb9153b4a792d5e
SWFullPotential <swfullpotential@gmail.com>
2020-12-16 14:09:45 -0700

    algorithm scripting practice

--- JS practice/codeCampJS.js
+++ JS practice/codeCampJS.js
@@ -548,7 +548,18 @@ function confirmEnding(str, target) {
 */
 confirmEnding("Bastian", "n");
 //----------
+//Repeat a given string str (first argument) for num times (second argument). Return an empty string if num is not a positive number. For the purpose of this challenge, do not use the built-in .repeat() method.
 
+function repeatStringNumTimes(str, num) {
+  let newStr = "";
+  while (num > 0) {
+    newStr += str;
+    num--;
+  }
+  return newStr;
+}
+
+repeatStringNumTimes("abc", 3);
 //----------
 
 //----------
