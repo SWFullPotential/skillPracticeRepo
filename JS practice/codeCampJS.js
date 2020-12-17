b3527d0c32fc87886df63702cbb291b600c691af
SWFullPotential <swfullpotential@gmail.com>
2020-12-17 13:38:36 -0700

    algorithm scripting practice"

--- JS practice/codeCampJS.js
+++ JS practice/codeCampJS.js
@@ -606,7 +606,14 @@ function titleCase(str) {
 
 titleCase("I'm a little tea pot");
 //----------
+//Copy each element of the first array into the second array, in order. Begin inserting elements at index n of the second array. Return the resulting array. The input arrays should remain the same after the function runs.
+function frankenSplice(arr1, arr2, n) {
+  let newArr = arr2.slice();
+  newArr.splice(n, 0, ...arr1);
+  return newArr;
+}
 
+frankenSplice([1, 2, 3], [4, 5, 6], 1);
 //----------
 
 //----------
