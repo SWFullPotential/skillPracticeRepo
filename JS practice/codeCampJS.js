eef922060a80f1517841bd64882a07128f0ae47b
SWFullPotential <swfullpotential@gmail.com>
2020-12-18 17:15:14 -0700

    scripting practice

--- JS practice/codeCampJS.js
+++ JS practice/codeCampJS.js
@@ -652,7 +652,18 @@ getIndexToIns([10, 20, 30, 40, 50], 35);
 getIndexToIns([10, 20, 30, 40, 50], 30);
 
 //----------
+//Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
 
+function mutation(arr) {
+  let test = arr[1].toLowerCase();
+  let target = arr[0].toLowerCase();
+  for (let i = 0; i < test.length; i++) {
+    if (target.indexOf(test[i]) < 0) return false;
+  }
+  return true;
+}
+
+mutation(["Hello", "hEy"]);
 //----------
 
 //----------
