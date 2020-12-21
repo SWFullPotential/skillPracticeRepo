ba374a4247ec8d96152e9fcdb05d9dcf30dbafb8
SWFullPotential <swfullpotential@gmail.com>
2020-12-21 11:25:25 -0700

    Object Oriented Programming

--- JS practice/codeCampJS.js
+++ JS practice/codeCampJS.js
@@ -1024,7 +1024,15 @@ let glideMixin = function (obj) {
 glideMixin(bird);
 glideMixin(boat);
 //----------
+//Change how weight is declared in the Bird function so it is a private variable. Then, create a method getWeight that returns the value of weight 15.
 
+function Bird() {
+  let weight = 15;
+
+  this.getWeight = function () {
+    return weight;
+  };
+}
 //----------
 
 //----------
