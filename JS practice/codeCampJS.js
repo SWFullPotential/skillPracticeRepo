0b74fc7d2611e0cb6dcff8e2c856621ee53e0e64
SWFullPotential <swfullpotential@gmail.com>
2020-12-21 10:14:12 -0700

    Object Oriented Programming

--- JS practice/codeCampJS.js
+++ JS practice/codeCampJS.js
@@ -729,7 +729,16 @@ function Dog() {
   this.numLegs = 4;
 }
 //----------
+//Use the Dog constructor from the last lesson to create a new instance of Dog, assigning it to a variable hound.
 
+function Dog() {
+  this.name = "Rupert";
+  this.color = "brown";
+  this.numLegs = 4;
+}
+// Only change code below this line
+
+let hound = new Dog();
 //----------
 
 //----------
