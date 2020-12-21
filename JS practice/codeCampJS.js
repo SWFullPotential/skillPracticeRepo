ce02140264838d73cae4b2c27483c7fae4025f39
SWFullPotential <swfullpotential@gmail.com>
2020-12-21 11:27:14 -0700

    Object Oriented Programming

--- JS practice/codeCampJS.js
+++ JS practice/codeCampJS.js
@@ -1034,6 +1034,11 @@ function Bird() {
   };
 }
 //----------
+//Rewrite the function makeNest and remove its call so instead it's an anonymous immediately invoked function expression (IIFE).
+
+(function () {
+  console.log("A cozy nest is ready");
+})();
 
 //----------
 
