01a5cf2ccee3549591414158bc0e4b6b144b65f6
SWFullPotential <swfullpotential@gmail.com>
2020-12-21 10:51:09 -0700

    Object Oriented Programming

--- JS practice/codeCampJS.js
+++ JS practice/codeCampJS.js
@@ -857,7 +857,17 @@ Dog.prototype = {
   },
 };
 //----------
+//Use isPrototypeOf to check the prototype of beagle.
 
+function Dog(name) {
+  this.name = name;
+}
+
+let beagle = new Dog("Snoopy");
+
+// Only change code below this line
+
+Dog.prototype.isPrototypeOf(beagle);
 //----------
 
 //----------
