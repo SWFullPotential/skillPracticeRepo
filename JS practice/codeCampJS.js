2343dcd4a7a4fe9400ec54f06207f609736471f4
SWFullPotential <swfullpotential@gmail.com>
2020-12-21 11:11:09 -0700

    Object Oriented Programming

--- JS practice/codeCampJS.js
+++ JS practice/codeCampJS.js
@@ -980,7 +980,27 @@ Dog.prototype.bark = function () {
 
 let beagle = new Dog();
 //----------
+//Override the fly() method for Penguin so that it returns "Alas, this is a flightless bird."
 
+function Bird() {}
+
+Bird.prototype.fly = function () {
+  return "I am flying!";
+};
+
+function Penguin() {}
+Penguin.prototype = Object.create(Bird.prototype);
+Penguin.prototype.constructor = Penguin;
+
+// Only change code below this line
+Penguin.prototype.fly = function () {
+  return "Alas, this is a flightless bird.";
+};
+
+// Only change code above this line
+
+let penguin = new Penguin();
+console.log(penguin.fly());
 //----------
 
 //----------
