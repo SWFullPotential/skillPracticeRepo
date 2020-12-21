427dc7159a083808d1cbcbaf076b9ae70fabe69f
SWFullPotential <swfullpotential@gmail.com>
2020-12-21 11:14:08 -0700

    Object Oriented Programming

--- JS practice/codeCampJS.js
+++ JS practice/codeCampJS.js
@@ -1002,7 +1002,27 @@ Penguin.prototype.fly = function () {
 let penguin = new Penguin();
 console.log(penguin.fly());
 //----------
+//Create a mixin named glideMixin that defines a method named glide. Then use the glideMixin to give both bird and boat the ability to glide.
 
+let bird = {
+  name: "Donald",
+  numLegs: 2,
+};
+
+let boat = {
+  name: "Warrior",
+  type: "race-boat",
+};
+
+// Only change code below this line
+let glideMixin = function (obj) {
+  obj.glide = function () {
+    console.log("Glidin away!");
+  };
+};
+
+glideMixin(bird);
+glideMixin(boat);
 //----------
 
 //----------
