69830b759a889e3beca0d06143f8d19d7eed2c5b
SWFullPotential <swfullpotential@gmail.com>
2020-12-21 10:45:49 -0700

    Object Oriented Programming

--- JS practice/codeCampJS.js
+++ JS practice/codeCampJS.js
@@ -822,7 +822,22 @@ function joinDogFraternity(candidate) {
   }
 }
 //----------
+//Add the property numLegs and the two methods eat() and describe() to the prototype of Dog by setting the prototype to a new object.
 
+function Dog(name) {
+  this.name = name;
+}
+
+Dog.prototype = {
+  // Only change code below this line
+  numLegs: 4,
+  eat: function () {
+    console.log("monch monch monch");
+  },
+  describe: function () {
+    console.log("My name is " + this.name + ".");
+  },
+};
 //----------
 
 //----------
