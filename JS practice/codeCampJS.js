e11d4b73364f5197e923f2a2f2455cc997b80ade
SWFullPotential <swfullpotential@gmail.com>
2020-12-21 10:28:36 -0700

    Object Oriented Programming

--- JS practice/codeCampJS.js
+++ JS practice/codeCampJS.js
@@ -750,7 +750,24 @@ function Dog(name, color) {
 
 let terrier = new Dog("Andrew", "white");
 //----------
+//Add the own properties of canary to the array ownProps.
 
+function Bird(name) {
+  this.name = name;
+  this.numLegs = 2;
+}
+
+let canary = new Bird("Tweety");
+let ownProps = [];
+// Only change code below this line
+
+for (let property in canary) {
+  if (canary.hasOwnProperty(property)) {
+    ownProps.push(property);
+  }
+}
+
+console.log(ownProps);
 //----------
 
 //----------
