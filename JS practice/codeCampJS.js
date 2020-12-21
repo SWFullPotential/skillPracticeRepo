a1d3b0590d196411796cb52ec2c90cdb50846d95
SWFullPotential <swfullpotential@gmail.com>
2020-12-21 09:31:31 -0700

    Object Oriented Programming: Create a Basic JavaScript Object

--- JS practice/codeCampJS.js
+++ JS practice/codeCampJS.js
@@ -688,7 +688,15 @@ let dog = {
 };
 //Create a dog object with name and numLegs properties, and set them to a string and a number, respectively.
 //----------
+//Print both properties of the dog object to your console.
 
+let dog = {
+  name: "Spot",
+  numLegs: 4,
+};
+// Only change code below this line
+console.log(dog.name);
+console.log(dog.numLegs);
 //----------
 
 //----------
