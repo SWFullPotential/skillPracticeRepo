9baa8945e181e692714f7637030db8d267b0c05d
SWFullPotential <swfullpotential@gmail.com>
2020-12-21 10:58:20 -0700

    Object Oriented Programming

--- JS practice/codeCampJS.js
+++ JS practice/codeCampJS.js
@@ -882,7 +882,32 @@ Dog.prototype.isPrototypeOf(beagle); // yields true
 // Fix the code below so that it evaluates to true
 Object.prototype.isPrototypeOf(Dog.prototype);
 //----------
+//The eat method is repeated in both Cat and Bear. Edit the code in the spirit of DRY by moving the eat method to the Animal supertype.
 
+function Cat(name) {
+  this.name = name;
+}
+
+Cat.prototype = {
+  constructor: Cat,
+};
+
+function Bear(name) {
+  this.name = name;
+}
+
+Bear.prototype = {
+  constructor: Bear,
+};
+
+function Animal() {}
+
+Animal.prototype = {
+  constructor: Animal,
+  eat: function () {
+    console.log("nom nom nom");
+  },
+};
 //----------
 
 //----------
