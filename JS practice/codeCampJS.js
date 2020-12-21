df0412092c025cc5ff86d5c507acc58e88916d4b
SWFullPotential <swfullpotential@gmail.com>
2020-12-21 10:42:34 -0700

    Object Oriented Programming

--- JS practice/codeCampJS.js
+++ JS practice/codeCampJS.js
@@ -807,7 +807,20 @@ for (let property in beagle) {
 console.log(ownProps);
 console.log(prototypeProps);
 //----------
+//Write a joinDogFraternity function that takes a candidate parameter and, using the constructor property, return true if the candidate is a Dog, otherwise return false.
 
+function Dog(name) {
+  this.name = name;
+}
+
+// Only change code below this line
+function joinDogFraternity(candidate) {
+  if (candidate.constructor === Dog) {
+    return true;
+  } else {
+    return false;
+  }
+}
 //----------
 
 //----------
