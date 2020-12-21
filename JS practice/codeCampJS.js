56c20f2a6bdbf6a712a891dea0abf7658db8d9da
SWFullPotential <swfullpotential@gmail.com>
2020-12-21 11:03:45 -0700

    Object Oriented Programming

--- JS practice/codeCampJS.js
+++ JS practice/codeCampJS.js
@@ -925,7 +925,23 @@ Animal.prototype = {
 let duck = Object.create(Animal.prototype); // Change this line
 let beagle = Object.create(Animal.prototype); // Change this line
 //----------
+//Modify the code so that instances of Dog inherit from Animal.
 
+function Animal() {}
+
+Animal.prototype = {
+  constructor: Animal,
+  eat: function () {
+    console.log("nom nom nom");
+  },
+};
+
+function Dog() {}
+
+// Only change code below this line
+Dog.prototype = Object.create(Animal.prototype);
+
+let beagle = new Dog();
 //----------
 
 //----------
